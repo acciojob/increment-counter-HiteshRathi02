101f1948e224b57d1658469676ca925b6c8d66e0
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-07 15:25:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//your JS code here. If requir
+	const counterEl = document.getElementById('counter');
+    const buttonEl = document.getElementById('incrementBtn');
+
+    buttonEl.addEventListener('click', () => {
+      const currentValue = parseInt(counterEl.textContent, 10);
+      alert(currentValue); // Show alert before incrementing
+      counterEl.textContent = currentValue + 1;
+    });
